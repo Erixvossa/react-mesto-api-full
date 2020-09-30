6d119a9b24b74413bdde9a2aaea047aaae2b06ba
Alex Slomyanovskiy <erixvossa@yandex.ru>
2020-09-30 20:18:43 +0300

    15_1

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 
 const bodyParser = require('body-parser');
@@ -14,6 +13,8 @@ const {
 const { login, createUser } = require('./controllers/users.js');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 
@@ -60,17 +61,16 @@ app.post('/signup', celebrate({
 app.use(auth);
 
 app.use('/users', usersRouter);
+
 app.use('/cards', cardsRouter);
+
+app.use(() => {
+  throw new NotFoundError({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.use(errorLogger);
 
 app.use(errors());
-
-app.use((req, res) => {
-  res
-    .status(404)
-    .send({ message: 'Запрашиваемый ресурс не найден' });
-});
 
 app.use((err, req, res, next) => {
   if (err.status === '500' || err.status === undefined) {
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,114 +1,61 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const Card = require('../models/card');
 const BadRequestError = require('../errors/BadRequestError');
 const NotFoundError = require('../errors/NotFoundError');
-const TokenError = require('../errors/TokenError');
-const ConflictError = require('../errors/ConflictError');
+const DeleteCardError = require('../errors/DeleteCardError');
 
-module.exports.getAllUsers = (req, res, next) => {
-  User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch(next);
-};
+module.exports.createCard = (req, res, next) => {
+  const { name, link } = req.body;
 
-module.exports.getUser = (req, res, next) => {
-  User.findById(req.params._id)
-    .orFail()
-    .catch(() => {
-      throw new NotFoundError({ message: `Пользователь с идентификатором ${req.params.id} не найден` });
-    })
-    .then((user) => res.send({ data: user }))
-    .catch(next);
-};
-
-module.exports.createUser = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-    .then((hash) => User.create({
-      email: req.body.email,
-      password: hash,
-      name: req.body.name ? req.body.name : undefined,
-      avatar: req.body.avatar ? req.body.avatar : undefined,
-      about: req.body.about ? req.body.about : undefined,
-    }))
+  Card.create({ name, link, owner: req.user._id })
     .catch((err) => {
-      if (err.name === 'MongoError') {
-        throw new ConflictError({ message: 'Пользователь с таким email уже существует' });
-      }
       throw new BadRequestError({ message: `Запрос некорректен: ${err.message}` });
     })
-    .then((user) => {
-      res.send({
-        data: {
-          _id: user._id,
-          email: user.email,
-          name: user.name ? user.name : undefined,
-          avatar: user.avatar ? user.avatar : undefined,
-          about: user.about ? user.about : undefined,
-        },
-      });
-    })
+    .then((card) => res.send({ data: card }))
     .catch(next);
 };
 
-module.exports.updateUser = (req, res, next) => {
-  const { name, about } = req.body;
+module.exports.getAllCards = (req, res, next) => {
+  Card.find({})
+    .populate('user')
+    .then((cards) => res.send({ data: cards }))
+    .catch(next);
+};
+
+module.exports.deleteCard = (req, res, next) => {
   const currentOwner = req.user._id;
-  User.findByIdAndUpdate(req.user._id,
-    { name, about },
-    {
-      new: true,
-      runValidators: true,
-    })
-    .orFail()
-    .catch((err) => {
-      if (err instanceof NotFoundError) {
-        throw err;
+  Card.findByIdAndDelete(req.params._id)
+    .then((card) => {
+      if (card.owner !== currentOwner) {
+        throw new DeleteCardError({ message: 'Запрос некорректен: недостаточно прав' });
       }
-      throw new BadRequestError({ message: `Запрос некорректен: ${err.message}` });
-    })
-    .then((updatedUser) => {
-      if (currentOwner !== updatedUser._id) {
-        throw new BadRequestError({ message: 'Запрос некорректен' });
+      if (!card) {
+        throw new NotFoundError({ message: `Kарточка с идентификатором ${req.params.id} не найдена` });
       }
-      res.send({ data: updatedUser });
+      res.send({ data: card });
     })
     .catch(next);
 };
 
-module.exports.updateAvatar = (req, res, next) => {
-  const { avatar } = req.body;
-
-  User.findByIdAndUpdate(req.user._id,
-    { avatar },
-    {
-      new: true,
-      runValidators: true,
-    })
+module.exports.likeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(req.params._id,
+    { $addToSet: { likes: req.user._id } },
+    { new: true })
     .orFail()
-    .catch((err) => {
-      if (err instanceof NotFoundError) {
-        throw err;
-      }
-      throw new BadRequestError({ message: `Запрос некорректен: ${err.message}` });
-    })
-    .then((updatedAvatar) => res.send({ data: updatedAvatar }))
-    .catch(next);
-};
-
-module.exports.login = (req, res, next) => {
-  const { email, password } = req.body;
-  return User.findUserByCredentials(email, password)
     .catch(() => {
-      throw new TokenError({ message: `Пользователь с идентификатором ${req.body.email} не найден` });
+      throw new NotFoundError({ message: `Kарточка с идентификатором ${req.params.id} не найдена` });
     })
-    .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        'some-secret-key',
-        { expiresIn: '7d' },
-      );
-      res.send({ token });
+    .then((likes) => res.send({ data: likes }))
+    .catch(next);
+};
+
+module.exports.dislikeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(req.params._id,
+    { $pull: { likes: req.user._id } },
+    { new: true })
+    .orFail()
+    .catch(() => {
+      throw new NotFoundError({ message: `Kарточка с идентификатором ${req.params.id} не найдена` });
     })
+    .then((likes) => res.send({ data: likes }))
     .catch(next);
 };
